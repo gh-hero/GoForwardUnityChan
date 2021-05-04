013aaad0e603c792c2dbd3e0b289ae47e95901f0
gh-hero <75409660+gh-hero@users.noreply.github.com>
2021-05-04 16:24:29 +0900

    Initial commit

---
+++ Assets/BlockSE.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSE : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //他のオブジェクトと接触した場合の処理
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Block" || other.gameObject.tag == "Road")
+        {
+
+            //効果音を鳴らす
+            GetComponent<AudioSource>().Play();
+
+        }
+
+    }
+
+}
